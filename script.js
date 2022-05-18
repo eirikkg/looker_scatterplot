4923c358608963dd412e1c07e295bbcecd27220e
eirikkg <72858061+eirikkg@users.noreply.github.com>
2022-05-18 13:12:11 +0200

    details

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var chart;
 looker.plugins.visualizations.add({
   // Id and Label are legacy properties that no longer have any function besides documenting
@@ -10,18 +9,13 @@ looker.plugins.visualizations.add({
     font_size: {
       type: "string",
       label: "Font Size",
-      values: [
-        {"Large": "large"},
-        {"Small": "small"}
-      ],
+      values: [{ Large: "large" }, { Small: "small" }],
       display: "radio",
-      default: "large"
-    }
+      default: "large",
+    },
   },
   // Set up the initial state of the visualization
-  create: function(element, config) {
-
-
+  create: function (element, config) {
     element.innerHTML = `
       <style>
         .hello-world-vis {
@@ -45,15 +39,12 @@ looker.plugins.visualizations.add({
     chart = anychart.scatter();
     chart.container("container");
 
-  // initiate drawing the chart
-
-
+    // initiate drawing the chart
 
     console.log(config);
     console.log(element);
 
     // Insert a <style> tag with some styles we'll use later.
-
 
     // Create a container element to let us center the text.
     var container = element.appendChild(document.createElement("div"));
@@ -61,33 +52,35 @@ looker.plugins.visualizations.add({
 
     // Create an element to contain the text.
     this._textElement = container.appendChild(document.createElement("div"));
-
   },
   // Render in response to the data or settings changing
-  updateAsync: function(data, element, config, queryResponse, details, done) {
+  updateAsync: function (data, element, config, queryResponse, details, done) {
+    console.log("DETAILS");
+    console.log(details);
 
-
-    options = {}
-     // Create an option for each measure in your query
-     queryResponse.fields.measure_like.forEach(function(field) {
-       id = "color_" + field.name
-       options[id] =
-       {
-  label: field.label_short + " Color",
-  default: "#8B7DA8",
-  section: "Style",
-  type: "string",
-  display: "color"
-       }
-     })
-     this.trigger('registerOptions', options) // register options with parent page to update visConfig
+    options = {};
+    // Create an option for each measure in your query
+    queryResponse.fields.measure_like.forEach(function (field) {
+      id = "color_" + field.name;
+      options[id] = {
+        label: field.label_short + " Color",
+        default: "#8B7DA8",
+        section: "Style",
+        type: "string",
+        display: "color",
+      };
+    });
+    this.trigger("registerOptions", options); // register options with parent page to update visConfig
 
     // Clear any errors from previous updates
     this.clearErrors();
 
     // Throw some errors and exit if the shape of the data isn't what this chart needs
     if (queryResponse.fields.dimensions.length === 0) {
-      this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
+      this.addError({
+        title: "No Dimensions",
+        message: "This chart requires dimensions.",
+      });
       return;
     }
     console.log(queryResponse);
@@ -101,49 +94,47 @@ looker.plugins.visualizations.add({
     //this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell + " Hei");
 
     data_3 = [];
-  data.forEach((row)=>{
-    data_3.push({x: row[queryResponse.fields.dimensions[0].name].value , y:row[queryResponse.fields.dimensions[1].name].value});
-  });
+    data.forEach((row) => {
+      data_3.push({
+        x: row[queryResponse.fields.dimensions[0].name].value,
+        y: row[queryResponse.fields.dimensions[1].name].value,
+      });
+    });
 
-  console.log(data_3);
-
-
+    console.log(data_3);
 
     var data_1 = [
-      {x: 0.6, value: 22},
-      {x: 1.7, value: 55},
-      {x: 1.7, value: 80},
-      {x: 2.3, value: 50},
-      {x: 3.5, value: 80},
-      {x: 3.9, value: 74},
-      {x: 4, value: 68},
-      {x: 4, value: 76},
-      {x: 4.1, value: 84},
-      {x: 4.7, value: 93}
+      { x: 0.6, value: 22 },
+      { x: 1.7, value: 55 },
+      { x: 1.7, value: 80 },
+      { x: 2.3, value: 50 },
+      { x: 3.5, value: 80 },
+      { x: 3.9, value: 74 },
+      { x: 4, value: 68 },
+      { x: 4, value: 76 },
+      { x: 4.1, value: 84 },
+      { x: 4.7, value: 93 },
     ];
 
     // create data for the second series
     var data_2 = [
-      {x: 0.5, value: 17.5},
-      {x: 4.75, value: 100}
+      { x: 0.5, value: 17.5 },
+      { x: 4.75, value: 100 },
     ];
 
-  // create a chart
+    // create a chart
 
+    // create the first series (marker) and set the data
+    var series1 = chart.bubble(data_3);
 
-  // create the first series (marker) and set the data
-  var series1 = chart.bubble(data_3);
+    // create the second series (line) and set the data
+    //var series2 = chart.line(data_2);
+    //var series1 = chart.bubble(data_3);
+    chart.xAxis().title(queryResponse.fields.dimensions[0].name);
+    chart.yAxis().title(queryResponse.fields.dimensions[1].name);
+    chart.draw();
 
-  // create the second series (line) and set the data
-  //var series2 = chart.line(data_2);
-  //var series1 = chart.bubble(data_3);
-  chart.xAxis().title(queryResponse.fields.dimensions[0].name);
-  chart.yAxis().title(queryResponse.fields.dimensions[1].name);
-  chart.draw();
-
-  // set the container id
-
-
+    // set the container id
 
     // Set the size to the user-selected size
     if (config.font_size == "small") {
@@ -153,6 +144,6 @@ looker.plugins.visualizations.add({
     }
 
     // We are done rendering! Let Looker know.
-    done()
-  }
+    done();
+  },
 });
